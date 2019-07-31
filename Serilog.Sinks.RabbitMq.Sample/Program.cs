0f21ca9cdf92ef25262ec91aed096991a5824919
Ihar Yakimush <iharyakimush@gmail.com>
2019-07-31 19:43:21 +0300

    sampe project

--- Serilog.Sinks.RabbitMq.Sample/Program.cs
+++ Serilog.Sinks.RabbitMq.Sample/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using Serilog.Core;
 using Serilog.Formatting.Json;
 
@@ -12,6 +14,8 @@ namespace Serilog.Sinks.RabbitMq.Sample
 {
     class Program
     {
+        private static readonly object SyncObj = new object();
+
         static void Main(string[] args)
         {
             LoggerConfiguration loggerConfiguration = new LoggerConfiguration();
@@ -27,7 +31,7 @@ namespace Serilog.Sinks.RabbitMq.Sample
 
             Logger logger = loggerConfiguration.CreateLogger();
 
-            Subscriber.Subscribe("exc.logs.sample", "logsQueue", "#");
+            Subscribe("exc.logs.sample", "logsQueue", "#");
             
             logger.Information("Information sample");
             logger.Warning("Warning sample");
@@ -40,6 +44,33 @@ namespace Serilog.Sinks.RabbitMq.Sample
             ILogger<Program> msLogger = serviceProvider.GetRequiredService<ILogger<Program>>();
 
             msLogger.LogError("Some error");
+        }
+
+        public static void Subscribe(string exc, string queue, string routingKey)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.UserName = "rabbitmq";
+            factory.Password = "rabbitmq";
+
+            IConnection connection = factory.CreateConnection(new List<AmqpTcpEndpoint> { new AmqpTcpEndpoint("localhost", 5672) });
+
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare(queue, true, false, false);
+            channel.QueueBind(queue, exc, routingKey);
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (o, e) =>
+            {
+                lock (SyncObj)
+                {
+                    Console.WriteLine($"{e.RoutingKey}: {e.Body.Length}bytes");
+                }
+
+                // Confirm that message processed successfully
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            channel.BasicConsume(queue, false, consumer);
         }
     }
 }
