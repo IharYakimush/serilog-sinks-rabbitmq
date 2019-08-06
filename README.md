8c144a687ad230a948b6d2c4402ee12f3a541e63
Ihar Yakimush <iharyakimush@gmail.com>
2019-08-06 19:44:51 +0300

    audit sample

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
 # serilog-sinks-rabbitmq
 Serilog RabbitMQ Sink
 # Samples
+
+### WriteTo
 ```
 LoggerConfiguration loggerConfiguration = new LoggerConfiguration();
     loggerConfiguration.WriteTo.RabbitMq(
-        options => options.ExchangeName = "exc.logs.sample",
+        options =>
+        {
+            options.ExchangeName = "exc.logs.sample";
+	    
+            // By default for WriteTo
+            options.ConfirmPublish = false;
+        },
+        new AmqpTcpEndpoint("localhost", 5672),
+        factory =>
+        {
+            factory.UserName = "rabbitmq";
+            factory.Password = "rabbitmq";
+        },
+        new JsonFormatter());
+```
+### AuditTo
+```
+LoggerConfiguration loggerConfiguration = new LoggerConfiguration();
+    loggerConfiguration.AuditTo.RabbitMq(
+        options =>
+        {
+            options.ExchangeName = "exc.audit.sample";
+
+            // By default for AuditTo
+            options.ConfirmPublish = true;                    
+            options.ConfirmPublishTimeout = TimeSpan.FromSeconds(5);
+        },
         new AmqpTcpEndpoint("localhost", 5672),
         factory =>
         {
--- Serilog.Sinks.RabbitMq.Sample/Program.cs
+++ Serilog.Sinks.RabbitMq.Sample/Program.cs
@@ -21,7 +21,30 @@ namespace Serilog.Sinks.RabbitMq.Sample
         {
             LoggerConfiguration loggerConfiguration = new LoggerConfiguration();
             loggerConfiguration.WriteTo.RabbitMq(
-                options => options.ExchangeName = "exc.logs.sample",
+                options =>
+                {
+                    options.ExchangeName = "exc.logs.sample";
+
+                    // By default for WriteTo
+                    options.ConfirmPublish = false;
+                },
+                new AmqpTcpEndpoint("localhost", 5672),
+                factory =>
+                {
+                    factory.UserName = "rabbitmq";
+                    factory.Password = "rabbitmq";
+                },
+                new JsonFormatter());
+
+            loggerConfiguration.AuditTo.RabbitMq(
+                options =>
+                {
+                    options.ExchangeName = "exc.audit.sample";
+
+                    // By default for AuditTo
+                    options.ConfirmPublish = true;                    
+                    options.ConfirmPublishTimeout = TimeSpan.FromSeconds(5);
+                },
                 new AmqpTcpEndpoint("localhost", 5672),
                 factory =>
                 {
@@ -33,6 +56,7 @@ namespace Serilog.Sinks.RabbitMq.Sample
             Logger logger = loggerConfiguration.CreateLogger();
 
             Subscribe("exc.logs.sample", "logsQueue", "#");
+            Subscribe("exc.audit.sample", "auditQueue", "#");
             
             logger.Information("Information sample");
             logger.Warning("Warning sample");
